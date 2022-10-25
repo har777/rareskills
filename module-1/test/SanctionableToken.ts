ebe1b4b803052443ac99ce4e0efc04ef1e2699d4
Harikrishnan Shaji <hihari777@gmail.com>
2022-10-25 22:23:13 +0530

    Add some basic tests for SanctionableToken to check if sanctions work

---
+++ module-1/test/SanctionableToken.ts
@@ -0,0 +1,70 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("SanctionableToken", function () {
+  async function deploySanctionableToken() {
+    const [deployer, user1, user2] = await ethers.getSigners();
+
+    const SanctionableToken = await ethers.getContractFactory("SanctionableToken");
+    const sanctionableToken = await SanctionableToken.deploy();
+
+    return { deployer, user1, user2, sanctionableToken };
+  }
+
+  describe("Sanctions", function () {
+    it("Sanction a user", async function () {
+      const { deployer, user1, sanctionableToken } = await loadFixture(deploySanctionableToken);
+
+      expect(await sanctionableToken.isSanctioned(user1.address)).to.equal(false);
+      await sanctionableToken.connect(deployer).sanction(user1.address);
+      expect(await sanctionableToken.isSanctioned(user1.address)).to.equal(true);
+    });
+
+    it("UnSanctioned a user", async function () {
+      const { deployer, user1, sanctionableToken } = await loadFixture(deploySanctionableToken);
+
+      expect(await sanctionableToken.isSanctioned(user1.address)).to.equal(false);
+      await sanctionableToken.connect(deployer).sanction(user1.address);
+      expect(await sanctionableToken.isSanctioned(user1.address)).to.equal(true);
+      await sanctionableToken.connect(deployer).unSanction(user1.address);
+      expect(await sanctionableToken.isSanctioned(user1.address)).to.equal(false);
+    });
+
+    it("Sanctioned user cannot send tokens", async function () {
+      const { deployer, user1, user2, sanctionableToken } = await loadFixture(deploySanctionableToken);
+
+      await sanctionableToken.connect(deployer).sanction(user1.address);
+
+      await expect(sanctionableToken.connect(user1).transfer(user2.address, 1))
+        .to.be.revertedWithCustomError(sanctionableToken, "AccountIsSanctioned")
+        .withArgs(user1.address);
+    });
+
+    it("Sanctioned user cannot receive tokens", async function () {
+      const { deployer, user1, user2, sanctionableToken } = await loadFixture(deploySanctionableToken);
+
+      await sanctionableToken.connect(deployer).sanction(user1.address);
+
+      await expect(sanctionableToken.connect(user2).transfer(user1.address, 1))
+        .to.be.revertedWithCustomError(sanctionableToken, "AccountIsSanctioned")
+        .withArgs(user1.address);
+    });
+
+    it("Sanctioning emits an Event", async function () {
+      const { deployer, user1, sanctionableToken } = await loadFixture(deploySanctionableToken);
+
+      await expect(sanctionableToken.connect(deployer).sanction(user1.address))
+        .to.emit(sanctionableToken, "Sanctioned")
+        .withArgs(user1.address);
+    });
+
+    it("UnSanctioning emits an Event", async function () {
+      const { deployer, user1, sanctionableToken } = await loadFixture(deploySanctionableToken);
+
+      await expect(sanctionableToken.connect(deployer).unSanction(user1.address))
+        .to.emit(sanctionableToken, "UnSanctioned")
+        .withArgs(user1.address);
+    });
+  });
+});

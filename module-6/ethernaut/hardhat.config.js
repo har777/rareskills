c12f1c5abfc3f11331b06b1d492a8d8b856881f2
Harikrishnan Shaji <hihari777@gmail.com>
2023-01-26 19:00:11 +0530

    Solve ethernaut 8

--- module-6/ethernaut/hardhat.config.js
+++ module-6/ethernaut/hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.5.17",
+      },
+    ],
+  },
 };

f52ffff73be562cc1a04ee2465c666538e406798
Harikrishnan Shaji <hihari777@gmail.com>
2023-01-29 23:37:36 +0530

    Solve ethernaut 18

--- module-6/ethernaut/contracts/Eighteen.sol
+++ module-6/ethernaut/contracts/Eighteen.sol
@@ -26,4 +26,21 @@ contract EthernautEighteenMagicNum {
 // https://ethernaut.openzeppelin.com/level/0xFe18db6501719Ab506683656AAf2F80243F8D0c0
 
 // Solution
+// This was really hard but I get how it works now.
+// I didn't implement a function selector. Just a contract that returns 42.
 //
+// Bytecode of contract returning 42: 602a60005260206000f3
+// PUSH1 0x2a
+// PUSH1 0x00
+// MSTORE
+// PUSH1 0x20
+// PUSH1 0x00
+// RETURN
+//
+// Final bytecode which deploys the above and returns address: 69602a60005260206000f3600052600a6016f3
+// PUSH10 0x602a60005260206000f3
+// PUSH1 0x00
+// MSTORE
+// PUSH1 0x0A
+// PUSH1 0x16
+// RETURN

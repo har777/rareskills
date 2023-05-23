839298989a5c716a309d37806ac738d1e862730a
Harikrishnan Shaji <hihari777@gmail.com>
2023-05-23 21:50:32 +0530

    Solved Week22Exercise2

--- module-11/test/Week22Exercise2.js
+++ module-11/test/Week22Exercise2.js
@@ -16,9 +16,16 @@ describe("Week22Exercise1", function () {
     it("Exploit worked", async function () {
       const { week22Exercise2 } = await loadFixture(deployToken);
 
-      expect(await week22Exercise2.something()).to.equal(false);
+      // The expected verifyingAddress has already called the challenge method on Optimism.
+      // We can just reuse those parameters here.
+      // See: https://optimistic.etherscan.io/tx/0x08e18539b6a2b45c74aa3eb4bc769a173baf87b3373437123c9498d72f02c2e2
+      const message = "attack at dawn";
+      const signature =
+        "0xe5d0b13209c030a26b72ddb84866ae7b32f806d64f28136cb5516ab6ca15d3c438d9e7c79efa063198fda1a5b48e878a954d79372ed71922003f847029bf2e751b";
 
-      expect(await week22Exercise2.something()).to.equal(true);
+      expect(await week22Exercise2.used(signature)).to.equal(false);
+      await week22Exercise2.challenge(message, signature);
+      expect(await week22Exercise2.used(signature)).to.equal(true);
     });
   });
 });
